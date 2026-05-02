71605fe2501d6e2821045f23b966416eb390ea24
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-05-02 17:59:45 +0530

    Delete CKD Project/ckdProject/forms.py

--- CKD Project/ckdProject/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-
-
-class ckdForm(forms.ModelForm):
-    class Meta():
-        model=ckdModel
-        fields=['Blood_Glucose_Random','Blood_Urea','Serum_Creatine','Packed_cell_volume','White_blood_count']
